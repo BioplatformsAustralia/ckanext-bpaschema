83523fea34b4f9a8098a0c07e69f63c747769d75
Mark Tearle <mark.tearle@qcif.edu.au>
2022-06-09 08:25:09 +0800

    Move schemas to new directory for ckanext-bpaschema, remove files from theme

--- ckanext/bpatheme/fanstatic/extra-drop-down.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.dropdown a.toggle-one').on("click", function (e) {
-        $(this).next('ul').toggle();
-        e.stopPropagation();
-        e.preventDefault();
-    });
-});
