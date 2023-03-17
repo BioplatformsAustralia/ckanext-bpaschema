ce7f5964d6b3eb409a238c8a9874ea5b67cff6e0
Brigette Gonch <b.gonch@qcif.edu.au>
2023-03-17 16:40:45 +1000

    Add new Schema ausarg-genomics-ddrad

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = []
 
 setup(
     name="ckanext-bpaschema",
-    version="4.0.11",
+    version="4.0.12",
     description='''CKAN extension to hold schemas for the Bioplatforms Australian Data Portal''',
     long_description=long_description,
     long_description_content_type="text/markdown",
