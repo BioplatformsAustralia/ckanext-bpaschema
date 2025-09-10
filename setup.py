122edda7bff962a1ed7ab1ccc30d4e5bdfc3b5fc
Brigette Gonch <b.gonch@qcif.edu.au>
2025-09-10 10:05:32 +1000

    Add pacbio-hifi datatype to collaborations for Cane Toads, bump version of schema to include them

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = []
 
 setup(
     name="ckanext-bpaschema",
-    version="4.4.2",
+    version="4.4.3",
     description='''CKAN extension to hold schemas for the Bioplatforms Australian Data Portal''',
     long_description=long_description,
     long_description_content_type="text/markdown",
